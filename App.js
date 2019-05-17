8d4fdadaf595cabc0d1e427e0d70bd24708e0ac6
Joao <joaoded@hotmail.com>
2019-05-17 10:58:59 -0300

    simple example

--- App.js
+++ App.js
@@ -1,43 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <View style={{backgroundColor: 'red', width: 50, height: 50}}></View>
-        <View style={{backgroundColor: 'green', width: 100, height: 100}}></View>
-        <View style={{backgroundColor: 'yellow', width: 150, height: 150}}></View>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+        <View style={{flex: 1, backgroundColor: 'blue'}}></View>      
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  }
 });
