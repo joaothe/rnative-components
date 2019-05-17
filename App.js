d5a78a19a2dcc6fa962141f80540757c39830606
Joao <joaoded@hotmail.com>
2019-05-17 11:37:48 -0300

    More additions

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end'}}>
         <View style={{width: 50, height: 50, backgroundColor: 'red'}}></View>
         <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>
         <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
