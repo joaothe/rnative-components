a02428629f386537537c7d299048793f92188335
Joao <joaoded@hotmail.com>
2019-05-17 12:34:59 -0300

    New component as an example

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'joao'
+    }
+  }
+
+  getName = (value) => {
+    this.setState({name: value})
+  }
+
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'red'}}></View>
-        <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>
-        <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Name"
+          onChangeText={this.getName}
+          underlineColorAndroid="transparent" />
+        <Text style={styles.textStyled}>{this.state.name}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#000000',
+    margin: 20,
+    padding: 10
+  },
+  textStyled: {
+    textAlign: 'center',
+    fontSize: 17,
+    color: '#000000'
+  }
+})
