05c0d1f5fba855b5d1bb576c456297c59ce9d8b3
Joao <joaoded@hotmail.com>
2019-05-20 17:12:46 -0300

    Async storage simple example

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
 import React, {Component} from 'react'
-import {Platform, StyleSheet, View, Text, FlatList, Picker, Slider} from 'react-native'
+import {Platform, StyleSheet, View, Text, TextInput, AsyncStorage} from 'react-native'
 
 export default class App extends Component {
+
+  //Async storage: consegue gravar algo no celular, e se quiser fechar o app, ele mostra o que foi salvo antes
 
   constructor(props) {
     super(props)
     this.state = {
-      valor: 10
+      nome: ''
     }
+
+    this.gravarNome = this.gravarNome.bind(this)
+
+    AsyncStorage.getItem("nomeDigitado").then((value) => {
+      this.setState({nome: value})
+    })
+  }
+  
+  gravarNome(text) {
+    this.setState({nome: text})
+
+    AsyncStorage.setItem("nomeDigitado", text)
   }
 
   render() {
 
     return (
       <View style={styles.container}>
-        <Slider minimumValue={0}
-                maximumValue={100}
-                onValueChange={(val) => this.setState({valor: val})}
-                value={this.state.valor} />
-        <Text style={{textAlign: 'center'}}>{this.state.valor}</Text>        
+        <TextInput style={styles.input}
+                   value={this.state.nome}
+                   onChangeText={(text) => this.gravarNome(text)}
+                   underlineColorAndroid="transparent" />
+        <Text style={styles.nome}>{this.state.nome}</Text>        
       </View>
     );
   }
@@ -27,6 +41,19 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container:{
     paddingTop: 20,
-    flex: 1
+    flex: 1,
+    alignItems: 'center'
+  },
+  input:{
+    width: 350,
+    height: 40,
+    borderColor: '#000000',
+    padding: 10,
+    borderWidth: 1
+  },
+  nome:{
+    fontSize: 25,
+    textAlign: 'center',
+    paddingTop: 20
   }
 })
