5f213fca6a37f426e295a9c5afdf29f8b9aa873e
Joao <joaoded@hotmail.com>
2019-05-20 22:02:08 -0300

    Minor fix

--- App.js
+++ App.js
@@ -1,38 +1,22 @@
 import React, {Component} from 'react'
-import {Platform, StyleSheet, View, Text, TextInput, AsyncStorage} from 'react-native'
+import {Platform, StyleSheet, View, Text, Button, Modal} from 'react-native'
 
 export default class App extends Component {
-
-  //Async storage: consegue gravar algo no celular, e se quiser fechar o app, ele mostra o que foi salvo antes
 
   constructor(props) {
     super(props)
     this.state = {
-      nome: ''
+      modalVisible: false
     }
-
-    this.gravarNome = this.gravarNome.bind(this)
-
-    AsyncStorage.getItem("nomeDigitado").then((value) => {
-      this.setState({nome: value})
-    })
   }
-  
-  gravarNome(text) {
-    this.setState({nome: text})
 
-    AsyncStorage.setItem("nomeDigitado", text)
-  }
+  abrir = () => {}
 
   render() {
 
     return (
       <View style={styles.container}>
-        <TextInput style={styles.input}
-                   value={this.state.nome}
-                   onChangeText={(text) => this.gravarNome(text)}
-                   underlineColorAndroid="transparent" />
-        <Text style={styles.nome}>{this.state.nome}</Text>        
+        <Button title="Entrar" onPress={this.abrir} />
       </View>
     );
   }
@@ -40,20 +24,9 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container:{
-    paddingTop: 20,
     flex: 1,
-    alignItems: 'center'
-  },
-  input:{
-    width: 350,
-    height: 40,
-    borderColor: '#000000',
-    padding: 10,
-    borderWidth: 1
-  },
-  nome:{
-    fontSize: 25,
-    textAlign: 'center',
-    paddingTop: 20
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DDD'
   }
 })
