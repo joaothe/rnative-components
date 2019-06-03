aa64d95f12c5181e3621f91d6df09878b9cc19ff
Joao <joaoded@hotmail.com>
2019-06-03 16:31:20 -0300

    Starting the usage of animations

--- App.js
+++ App.js
@@ -1,34 +1,30 @@
-import React, {Component} from 'react'
-import {Platform, StyleSheet, View, Text, Button, Modal} from 'react-native'
+import React, { Component } from 'react'
+import { Platform, StyleSheet, View, Text, Animated } from 'react-native'
 
 export default class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      modalVisible: false
+      LarAnimada: 150,
+      AltAnimada: 50
     }
   }
-
-  abrir = () => {}
 
   render() {
 
     return (
       <View style={styles.container}>
-        <Button title="Abrir" onPress={this.abrir} />
-        <Modal animationType="slide" visible={this.state.modalVisible}>
-          <View>
-            <Text>Seja bem-vindo!</Text>
-          </View>
-        </Modal>
+        <Animated.View style={{width: this.state.LarAnimada, height: this.state.AltAnimada, backgroundColor: 'blue', justifyContent: 'center'}}>
+          <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>Carregando...</Text>
+        </Animated.View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App';
 import {name as appName} from './app.json';
 import Routes from "./src";
 
-AppRegistry.registerComponent(appName, () => Routes);
+AppRegistry.registerComponent(appName, () => App);
