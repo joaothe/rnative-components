c040deede5d8a789371490f90904e3525afa376d
Joao <joaoded@hotmail.com>
2019-06-03 17:28:51 -0300

    Parallel example

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
       AltAnimada: new Animated.Value(50)
     }
 
-    Animated.sequence([
+    Animated.parallel([
       Animated.timing(
         this.state.LarAnimada,
         {
