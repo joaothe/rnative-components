6343959ca870594521a19a166e263a6d6b0927c7
Joao <joaoded@hotmail.com>
2019-06-20 17:12:40 -0300

    Getting back with simple template

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, View, Text, Animated } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 
 export default class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      LarAnimada: new Animated.Value(150),
-      AltAnimada: new Animated.Value(50)
-    }
-
-    Animated.parallel([
-      Animated.timing(
-        this.state.LarAnimada,
-        {
-          toValue: 300,
-          duration: 2000
-        }
-      ),
-      Animated.timing(
-        this.state.AltAnimada,
-        {
-          toValue: 200,
-          duration: 2000
-        }
-      )
-    ]).start()
-
-  }
 
   render() {
 
     return (
       <View style={styles.container}>
-        <Animated.View style={{width: this.state.LarAnimada, height: this.state.AltAnimada, backgroundColor: 'blue', justifyContent: 'center'}}>
-          <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>Carregando...</Text>
-        </Animated.View>
+        <Text style={{color: 'black', fontSize: 22, textAlign: 'center'}}>Hello</Text>
       </View>
     );
   }
@@ -44,8 +16,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#DDD'
+    justifyContent: 'center'
   }
 })
